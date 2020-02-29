9038c79402879725a38e8e766205928b0dcfdb37
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-02-29 13:14:59 -0300

    criando um DAO para a classe contato

---
+++ src/br/com/caelum/jdbc/modelo/Contato.java
@@ -0,0 +1,45 @@
+package br.com.caelum.jdbc.modelo;
+
+import java.util.Calendar;
+
+public class Contato {
+//o.	Deverá	ter	id,	nome,	email,	endereço	e	uma	data	de	nascimento.
+	private Long id;
+	private String nome;
+	private String email;
+	private String endereco;
+	private Calendar dataNascimento;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Calendar dataNacimento) {
+		this.dataNascimento = dataNacimento;
+	}
+	
+	
+}
